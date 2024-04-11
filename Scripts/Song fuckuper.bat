19e2376e26f28b0118ec24677acd3d8fa9c90851
Lyall <58636255+Lyall-A@users.noreply.github.com>
2024-04-12 00:05:55 +0100

    Create Song fuckuper.bat

---
+++ Scripts/Song fuckuper.bat
@@ -0,0 +1,12 @@
+set bitrate="64B"
+set filters="volume=50dB"
+
+set opusSave=%1.opus
+set fuckedupSave=%1-fuckedup.mp3
+
+ffmpeg -i %1 -c:a libopus -b:a %bitrate% -filter:a "%filters%" -map_metadata -1 %opusSave%
+ffmpeg -i %1.opus %fuckedupSave% -y
+
+del %opusSave%
+
+call %fuckedupSave%
